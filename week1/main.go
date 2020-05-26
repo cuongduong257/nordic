3670a489bcdaced1c05ab73a05823261b6c34abc
CuongDT <cuongdt@sendo.vn>
2020-05-26 17:49:10 +0700

    Done

--- week1/main.go
+++ week1/main.go
@@ -18,16 +18,16 @@ type Ward struct {
 
 // District : Define Districts
 type District struct {
-	ID    string `yaml:"id,omitempty"`
-	Name  string `yaml:"name"`
-	Wards []Ward
+	ID    string  `yaml:"id,omitempty"`
+	Name  string  `yaml:"name"`
+	Wards []*Ward `yaml:"wards"`
 }
 
 // City : Define City
 type City struct {
-	ID        string `yaml:"id,omitempty"`
-	Name      string `yaml:"name"`
-	Districts []District
+	ID        string      `yaml:"id"`
+	Name      string      `yaml:"name"`
+	Districts []*District `yaml:"districts"`
 }
 
 func check(e error) {
@@ -48,7 +48,10 @@ func main() {
 
 	r := csv.NewReader(input)
 	w := bufio.NewWriter(output)
-	citys := []City{}
+
+	data := []*City{}
+	cityMap := make(map[interface{}]*City)
+	districtMap := make(map[interface{}]*District)
 
 	for {
 		record, err := r.Read()
@@ -57,29 +60,41 @@ func main() {
 		}
 		check(err)
 
-		city := City{
-			ID:   record[1],
-			Name: record[0],
-			Districts: []District{
-				{
-					ID:   record[3],
-					Name: record[2],
-					Wards: []Ward{
-						{
-							ID:   record[5],
-							Name: record[4],
-						},
-					},
-				},
-			},
+		if record[0] == "Tỉnh Thành Phố" {
+			continue
 		}
-		citys = append(citys, city)
+
+		city, ok := cityMap[record[1]]
+		if !ok {
+			city = &City{
+				ID:   record[1],
+				Name: record[0],
+			}
+			cityMap[record[1]] = city
+			data = append(data, city)
+		}
+
+		district, ok := districtMap[record[3]]
+		if !ok {
+			district = &District{
+				ID:   record[3],
+				Name: record[2],
+			}
+			districtMap[record[3]] = district
+			city.Districts = append(city.Districts, district)
+		}
+
+		ward := &Ward{
+			ID:   record[5],
+			Name: record[4],
+		}
+		district.Wards = append(district.Wards, ward)
 	}
 
-	marshal, error := yaml.Marshal(citys)
+	marshal, error := yaml.Marshal(data)
 	check(error)
 	w.WriteString(string(marshal))
 
 	w.Flush()
-	fmt.Printf("Write done...")
+	fmt.Printf("Done...\n")
 }
